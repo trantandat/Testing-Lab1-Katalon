fa3b9c31e61e0774e0cde53b5b8860e8b5875bac
trantandat <tandat180599@gmail.com>
2020-10-23 21:59:00 +0700

    Update Lab 5

---
+++ My First Web UI Project/Test Suites/Profile TestSuites.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Profile TestSuites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>94bc831e-7a0f-42d3-b37c-20dc3fa8216d</testSuiteGuid>
+   <testCaseLink>
+      <guid>4485d0fe-bb4a-4a4b-b02c-0f71661b51be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Profile-TestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
